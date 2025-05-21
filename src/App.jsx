b30a6f4e929b6b94870ca1af5768cfffcb291e7f
SeHIgh <sehi0119@naver.com>
2025-05-21 20:22:01 +0900

    env(vite) : Vite 프로젝트 생성

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Dex } from "./pages/Dex";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Dex" element={<Dex />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ src/pages/Dex.jsx
@@ -0,0 +1,7 @@
+export const Dex = () => {
+    return (
+        <div>
+            <h1>Dex</h1>
+        </div>
+    );
+};
